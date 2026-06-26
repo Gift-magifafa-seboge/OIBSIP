eddafc6b63a13428a461cb11fa0e0955802401e1
Gift-magifafa-seboge <giftseboge2004@gmail.com>
2026-06-26 13:22:06 +0200

    feat: basic user input for number guessing game

--- Task2_NumberGuessingGame/NumberGuessingGame.java
+++ Task2_NumberGuessingGame/NumberGuessingGame.java
@@ -1,5 +1,22 @@
 package Task2_NumberGuessingGame;
 
+import java.util.Scanner;
+
 public class NumberGuessingGame {
+
+    public static void main(String[] args) {
+       
+         System.out.println("==========Number Guessing Game==========");
+         System.out.println("Guess a number between 1 to 1000");
+
+         Scanner input = new Scanner(System.in);
+         int guess = input.nextInt();
+
+         System.out.println("This is your guess:" + guess);
+ }
+
+   
+
+
     
 }

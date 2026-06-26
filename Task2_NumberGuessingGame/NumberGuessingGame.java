6bd9897263d92ca51a21c11fa2b9002023643544
Gift-magifafa-seboge <giftseboge2004@gmail.com>
2026-06-26 17:42:23 +0200

    feat: play again feature and the overall score

--- Task2_NumberGuessingGame/NumberGuessingGame.java
+++ Task2_NumberGuessingGame/NumberGuessingGame.java
@@ -7,6 +7,9 @@ public class NumberGuessingGame {
 
     public static void main(String[] args) {
        
+        int totalScore = 0;
+        char option = ' ';
+
         Scanner input = new Scanner(System.in);
          System.out.println("==========Number Guessing Game==========");
         
@@ -14,6 +17,7 @@ public class NumberGuessingGame {
          
 
          Random random = new Random();
+         do{
          int min = 1 , max = 1000, attempts = 0 , maxAttempts = 10;
          int randomNumber = min + random.nextInt(max);
 
@@ -41,16 +45,27 @@ public class NumberGuessingGame {
          }
          if(!answer)
          {
+            
             System.out.println("Game over!! You have used all your " + maxAttempts + " attempts");
             System.out.println("The correct answer is: " +  randomNumber);
          }else {
+            
             System.out.println("You have guessed it in" + attempts + " attempts");
 
             int score = (maxAttempts - attempts + 1) * 10;
             if(score<0)score = 0;
 
+            totalScore+=score;
+
             System.out.println("Your score" + score );
          }
+
+         System.out.println("Do you want to play again? Y/N");
+         option = input.next().charAt(0);
+        }while(Character.toUpperCase(option) == 'Y');
+
+        System.out.println("Thank you for playing!!");
+        System.out.println("Your total score is:" + totalScore);
 
 
  }
